3effb92fc0435caaafcf92efe2a53189239698cc
Md Tayam Hasan Mahim <134125244+mahim7531@users.noreply.github.com>
2025-11-25 14:40:54 +0600

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,24 @@
+
+
+const sideMenu=document.querySelector('#sideMenu')
+const navBar=document.querySelector("nav")
+const navLinks=document.querySelector("nav ul")
+
+function openMenu(){
+    sideMenu.style.transform='translate(-16rem)';
+}
+
+function closeMenu(){
+    sideMenu.style.transform='translate(16rem)';
+}
+
+window.addEventListener('scroll',()=>{
+    if(scrollY>50){
+        navBar.classList.add('bg-white','bg-opacity-50','backdroup-blur-lg','shadow-sm');
+        navLinks.classList.remove('bg-white','bg-opacity-50','shadow-sm');
+    }else{
+         navBar.classList.remove('bg-white','bg-opacity-50','backdroup-blur-lg','shadow-sm')
+           navLinks.classList.add('bg-white','bg-opacity-50','shadow-sm');
+
+    }
+})
